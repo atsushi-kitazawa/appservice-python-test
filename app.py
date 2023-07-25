eecdeefcea87294baac6dc6460cfaa2fe59094e8
atsushi-kitazawa <47269784+atsushi-kitazawa@users.noreply.github.com>
2023-07-25 02:07:25 +0000

    commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-    return "hello world. hello!"
+    print(os.environ)
+    return os.getenv('KEY1')
 
 ## 実行
 if __name__ == "__main__":
